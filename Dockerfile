947570ae525d5f05423f0a2fd2a03411420854b5
Vanderlei Pereira <33144973+vnderlev@users.noreply.github.com>
2019-03-06 18:53:32 -0300

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Dockerfile for example whisk docker action
+
+FROM ekidd/rust-musl-builder AS builder
+
+ADD . ./
+
+RUN sudo chown -R rust:rust /home/rust
+
+RUN cargo build --release
+
+# Now, we need to build our _real_ Docker container
+
+FROM openwhisk/dockerskeleton
+
+ENV FLASK_PROXY_PORT 8080
+
+RUN apk --no-cache add ca-certificates
+
+### Copy source file(s)
+COPY --from=builder \
+	/home/rust/src/target/x86_64-unknown-linux-musl/release/rusty_microservice \
+	/action/exec
+
+CMD ["/bin/bash", "-c", "cd actionProxy && python -u actionproxy.py"]
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+extern crate reqwest;
+extern crate rustc_serialize;
+extern crate openssl_probe;
+
+use std::env;
+use rustc_serialize::json;
+use rustc_serialize::json::Json;
+use reqwest::header::HeaderMap;
+use reqwest::header::CONTENT_TYPE;
+use reqwest::header::ACCEPT;
+use reqwest::Client;
+
+fn get_iam_token(apikey: &String) -> Result<String, reqwest::Error> {
+    let mut headers = HeaderMap::new();
+    headers.insert(CONTENT_TYPE, "application/x-www-form-urlencoded".parse().unwrap());
+    headers.insert(ACCEPT, "application/json".parse().unwrap());
+
+    let mut msg_body = String::from("grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=");
+    msg_body.push_str(apikey);
+    //println!("{}", msg_body);
+
+    let res = Client::new()
+        .post("https://iam.cloud.ibm.com/identity/token")
+        .headers(headers)
+        .body(msg_body)
+        .send()?
+        .text()?;
+    //println!("{}", res);
+
+    Ok(res)
+}
+
+fn main() {
+    openssl_probe::init_ssl_cert_env_vars();
+
+    let args: Vec<String> = env::args().collect();
+    //let apikey = String::from("3y9QjSOQxHRfpkxTtEIBAYW1B3M-FB4fh62gxu_aPy4T");
+    //println!("Size of Vec<String>: {}", args.len());
+
+    if args.len() == 1 {
+        println!("Empty argument.");
+    } else {
+        println!("{}", &args[1]);
+    }
+    //let apikey = &args[1];
+    //println("{}", apikey);
+    //println!("{}", get_iam_token(&apikey).unwrap());
+}
