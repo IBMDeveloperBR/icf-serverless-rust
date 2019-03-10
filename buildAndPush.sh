e2f71c1e28ebc633da78528b902cf4e85500aadc
Vanderlei Pereira <33144973+vnderlev@users.noreply.github.com>
2019-03-10 08:14:58 -0300

    Update buildAndPush.sh

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -21,19 +21,3 @@ if [ $? -ne 0 ]; then
     echo "Docker push failed"
 #    exit
 fi
-
-
-
-#### Steps
-### Rust
-# Just save the updated src and run the docker build
-
-### Docker
-# cd ./cargo-project-directory
-# docker build -t vmpereiraf/rust-serverless-skel:v0.1 .
-# docker push vmpereiraf/rust-serverless-skel:v0.1
-
-### OpenWhisk CLI
-# ibmcloud fn action create <action_name> --docker vmpereiraf/rust-serverless-skel ### to create new action
-# ibmcloud fn action invoke --result <action_name> --param <json_formatted_parameters>
-# ibmcloud fn action update <action_name> --docker vmpereiraf/rust-serverless-skel ### to update existing action
